52fbf1ced86a5f1bdcc4a262161a7975c368917e
qa7t1 <qa7t1@MyMonsterPC>
2023-05-06 22:33:19 +0500

    New File Added

--- src/New.java
+++ src/New.java
@@ -7,12 +7,12 @@
  *
  * @author qa7t1
  */
-public class Ali extends javax.swing.JFrame {
+public class New extends javax.swing.JFrame {
 
     /**
-     * Creates new form Ali
+     * Creates new form New
      */
-    public Ali() {
+    public New() {
         initComponents();
     }
 
@@ -58,20 +58,20 @@ public class Ali extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Ali.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(New.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Ali.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(New.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Ali.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(New.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Ali.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(New.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Ali().setVisible(true);
+                new New().setVisible(true);
             }
         });
     }
